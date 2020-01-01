dd1fc2a2fe73bfd351c985b6654fc6a55bf2d9cc
Sara <hey@iamsaravieira.com>
2020-01-01 15:22:15 +0000

    add babel

---
+++ webpack.build.config.js
@@ -0,0 +1,24 @@
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        use: [{ loader: "babel-loader" }],
+        include: path.resolve(__dirname, "src")
+      }
+    ]
+  },
+  target: "electron-renderer",
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "SVG to JSX"
+    }),
+    new webpack.DefinePlugin({
+      "process.env.NODE_ENV": JSON.stringify("production")
+    })
+  ]
+};
