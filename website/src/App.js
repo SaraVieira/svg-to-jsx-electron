6ad3159024e5023791bc49292e07d1515007e3ad
Sara <hey@iamsaravieira.com>
2020-01-01 20:20:04 +0000

    add website

---
+++ website/src/App.js
@@ -0,0 +1,46 @@
+import React from "react"
+import app from "./app.png"
+import logo from "./logo.png"
+import "./App.css"
+
+function App() {
+  return (
+    <main className="App">
+      <div>
+        <img src={logo} alt="logo" />
+        <h1>SVG to JSX</h1>
+        <h2>
+          Get in SVG files and you can get out SVG code ready for your React APP
+        </h2>
+        <h3>Please control+click to able to open</h3>
+        {navigator.platform === "MacIntel" ? (
+          <>
+            <a
+              href="https://github.com/SaraVieira/svg-to-jsx-electron/releases/download/v0.0.2/SVG.to.JSX.zip"
+              className="button"
+            >
+              Download
+            </a>
+            <a
+              href="https://github.com/SaraVieira/svg-to-jsx-electron"
+              className="button github"
+            >
+              Github
+            </a>
+          </>
+        ) : (
+          <a
+            href="https://github.com/SaraVieira/svg-to-jsx-electron/"
+            className="button"
+          >
+            Github
+          </a>
+        )}
+      </div>
+
+      <img className="app-image" src={app} alt="app screen" />
+    </main>
+  )
+}
+
+export default App
---
+++ website/src/index.js
@@ -0,0 +1,7 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import "./index.css"
+import "circular-std"
+import App from "./App"
+
+ReactDOM.render(<App />, document.getElementById("root"))
